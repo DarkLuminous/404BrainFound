3f9a12cbc7d4e6c5578a6643681b371f5db37239
Bea A. Lagayada <lagayada.bea23@gmail.com>
2026-01-17 20:39:03 +0800

    Fix: corrected the calculation in the subtraction function

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+# BASIC CALCULATOR FUNCTIONS
+# make user choose operation
+
 # ADDITION FUNCTION
-# SUB
+
+# SUBTRACTION FUNCTION
+def sub (x, y):
+    return x - y
+
+# MULTIPLICATION FUNCTIONLK
+def multiply (x, y)
+    return x * y
+
+# DIVISION FUNCTION
+
+# loop back to make user choose operation again
