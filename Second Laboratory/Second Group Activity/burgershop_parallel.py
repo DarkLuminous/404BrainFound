e7efa1d9e7e5ead693a3bf64a479540128a541f7
Tam <martinez.thomasgabriel@gmail.com>
2026-02-21 01:41:36 +0000

    Altered by Bao, added sequential version

--- Second Laboratory/Second Group Activity/burgershop_parallel.py
+++ Second Laboratory/Second Group Activity/burgershop_parallel.py
@@ -11,3 +11,21 @@ COOK_TIME = 0.5
 ASSEMBLE_TIME = 0.2
 PACK_TIME = 0.1
 
+def process_order_sequential(order):
+    time.sleep(INTAKE_TIME)
+    time.sleep(PAYMENT_TIME)
+    time.sleep(COOK_TIME)
+    time.sleep(ASSEMBLE_TIME)
+    time.sleep(PACK_TIME)
+
+
+def run_sequential(orders):
+    start_time = time.time()
+
+    for order in orders:
+        process_order_sequential(order)
+
+    end_time = time.time()
+    return end_time - start_time
+
+    
